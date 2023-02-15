b7de708a5d6d55627aacdc0daf3ff06b290f7581
Marcel Berner <marcelberner62@gmail.com>
2023-02-15 02:05:18 +0100

    add cinema page

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Layout.module.scss";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { BiArrowFromLeft } from "react-icons/bi";
+import Link from "next/link";
 
 import Searchbar from "./Searchbar";
 import UserButtons from "./UserButtons";
@@ -13,26 +15,69 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
+  console.log(currentRoute);
+
+  const [sidebarState, setSidebarState] = useState<boolean>(true);
+
+  const sidebarHideHandler = () => {
+    if (sidebarState) {
+      setSidebarState(false);
+      document.documentElement.style.setProperty(
+        "--sidebar-current",
+        "var(--sidebar-hide)"
+      );
+      localStorage.setItem("sidebar-state", "false");
+    } else {
+      setSidebarState(true);
+      document.documentElement.style.setProperty(
+        "--sidebar-current",
+        "var(--sidebar-show)"
+      );
+      localStorage.setItem("sidebar-state", "true");
+    }
+  };
+
+  useEffect(() => {
+    const state = localStorage.getItem("sidebar-state") === "true";
+    setSidebarState(state);
+    if (!state)
+      document.documentElement.style.setProperty(
+        "--sidebar-current",
+        "var(--sidebar-hide)"
+      );
+  }, []);
   return (
-    <div className={styles.page_layout}>
+    <div
+      id="layout"
+      className={`${styles.page_layout} ${
+        currentRoute === "/cinema" ? styles.active : ""
+      }`}
+    >
       <header className={styles.header}>
-        <Image
-          priority
-          src="/logo.svg"
-          className="logo"
-          height={40}
-          width={200}
-          alt="PlayerApp logo"
-        />
+        <Link href={"/"}>
+          <Image
+            priority
+            src="/logo.svg"
+            className={styles.logo}
+            height={40}
+            width={200}
+            alt="PlayerApp logo"
+          />
+        </Link>
         <nav className={styles.header__nav}>
           <Searchbar />
           <UserButtons />
         </nav>
       </header>
-      <div className={styles.sidebar}>
-        <PageNav />
-        <FriendsList />
-        <button className={styles.sidebar_button}>
+      <div
+        className={`${styles.sidebar} ${!sidebarState ? styles.active : ""}`}
+      >
+        <PageNav state={sidebarState} />
+        <FriendsList state={sidebarState} />
+        <button className={styles.sidebar_button} onClick={sidebarHideHandler}>
           <BiArrowFromLeft />
         </button>
       </div>
